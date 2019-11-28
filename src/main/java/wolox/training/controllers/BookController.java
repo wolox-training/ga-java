669e381eec2bf08b8d361d1321b1a7a0d3696fc2
hellfishg <hellfish.g@protonmail.com>
2019-11-28 12:06:38 -0300

    Fix 1 for PR greeting

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
+
   @GetMapping("/greeting")
   public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
     model.addAttribute("name", name);
