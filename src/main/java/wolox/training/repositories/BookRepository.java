3069131934aa63c04d34469ad65b7d7237d876a7
hellfishg <hellfish.g@protonmail.com>
2019-12-04 20:09:28 -0300

    Book crud (#5)
    * Create book entity
    
    * pull request template
    
    * fix GenerationType.IDENTITY for PR
    
    * PR fix delete setId()
    
    * Create bookRepository package
    
    * BookRespository interface
    
    * book-crud and exceptions
    
    * Fix for PR, add javaDocs
    
    * Fix for PR 1
    
    * Fix for PR 2 change exception
    
    * Fix for PR 2 change bookModel constructor location

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -10,4 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BookRepository extends JpaRepository<Optional, Long> {
 
     Optional findByAuthor(String author);
+    public Book findByTitle(String title);
 }
