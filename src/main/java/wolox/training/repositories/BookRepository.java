85ab146fd66c8e49b0a9d6ab3c56337a922c45c1
hellfishg <hellfish.g@protonmail.com>
2019-12-04 20:07:04 -0300

    Book repository (#4)
    * Create book entity
    
    * pull request template
    
    * fix GenerationType.IDENTITY for PR
    
    * PR fix delete setId()
    
    * Create bookRepository package
    
    * BookRespository interface
    
    * Fix for PR, add javaDocs
    
    * Fix for PR 2 change bookModel constructor location

---
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -0,0 +1,13 @@
+package wolox.training.repositories;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/** Represents an repository of books.
+ * @author German Asprino
+ */
+
+public interface BookRepository extends JpaRepository<Optional, Long> {
+
+    Optional findByAuthor(String author);
+}
