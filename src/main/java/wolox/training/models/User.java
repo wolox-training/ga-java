4871be829e6f7cb786bab4429d5e47432c7711fd
hellfishg <hellfish.g@protonmail.com>
2019-12-10 13:00:00 -0300

    Fix PR1

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -25,7 +25,7 @@ import javax.validation.constraints.NotBlank;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column
